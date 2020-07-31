85eeca049c32bfbe3e8864d8ab3e2f70c1e53906
Alejandro <snackbarchat@gmail.com>
2020-07-31 02:41:38 -0300

    move mongo_url to env

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const urlMongo = 'mongodb://heroku_p66mmkdp:hpjs99ao7ol6agbekut8cpbun1@ds139665.mlab.com:39665/heroku_p66mmkdp';
+const urlMongo = process.env.URL_MONGO;
 
 const config = { useNewUrlParser: true, useUnifiedTopology: true }
 
